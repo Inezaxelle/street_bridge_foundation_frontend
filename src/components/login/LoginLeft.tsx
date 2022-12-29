710c9efae7bb46c8638a78f06659a05813f86e25
unknown <inezaaxelle44@gmail.com>
2022-12-29 12:20:28 +0200

    [login page]

--- src/components/login/LoginLeft.tsx
+++ src/components/login/LoginLeft.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import logo from "../../images/sbf_logo.svg"
 interface LoginLeftProps {
     
 }
  
 const LoginLeft: React.FC<LoginLeftProps> = () => {
     return ( 
-        <div>
-            
+        <div className='w-1/3 h-screen bg-[#F9F5E8] pt-12 md:block hidden'>
+            <img src={logo} alt="" className='mx-auto' />
         </div>
      );
 }
--- src/components/login/LoginRight.tsx
+++ src/components/login/LoginRight.tsx
@@ -1,14 +1,40 @@
-import React from 'react'
-interface LoginRightProps {
-    
-}
- 
+import React from "react";
+import { useState } from "react";
+import logo from "../../images/sbf_logo.svg"
+interface LoginRightProps {}
+
 const LoginRight: React.FC<LoginRightProps> = () => {
-    return ( 
-        <div>
-            
+  const [email, setEmail] = useState("");
+  const handleLogin = () => {
+    console.log(email);
+  };
+  return (
+    <div className="bg-[#042144] h-screen md:w-2/3 w-full text-white">
+      <div className="flex-col flex items-center w-4/5 mx-auto mt-12">
+        <img src={logo} alt="logo" className="md:hidden" />
+        <span>LOGIN INTO YOUR ACCOUNT</span>
+        <div className="flex flex-col w-4/5 mb-6 mt-12">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)} className="outline-none text-black pl-2"/>
         </div>
-     );
-}
- 
+        <div className="flex flex-col w-4/5">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" className="text-black pl-2 outline-none" />
+        </div>
+        <button
+          onClick={handleLogin}
+          className="text-[#042144] bg-white w-4/5 mt-10 py-3 font-bold"
+        >
+          Login
+        </button>
+      </div>
+      <div className="w-4/5 flex mx-auto">
+        <span className="w-4/5 mx-auto mt-6 cursor-pointer">
+          Forgot Password?
+        </span>
+      </div>
+    </div>
+  );
+};
+
 export default LoginRight;
