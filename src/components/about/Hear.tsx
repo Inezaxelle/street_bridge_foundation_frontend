5dcbe52277241ce37cbdb4ac669cb48215ceab5f
unknown <inezaaxelle44@gmail.com>
2022-12-30 22:12:50 +0200

    [still about page]

--- src/components/about/Hear.tsx
+++ src/components/about/Hear.tsx
@@ -6,9 +6,13 @@ const Hear: React.FC<HearProps> = () => {
   return (
     <div className="bg-[#082244] py-8 lg:px-24 px-6">
       <div className="text-white mb-6">Hear From us</div>
-      <div className="flex">
-        <img src={pic} alt="sbf logo" className="w-1/2 bg-white h-[522px]" />
-        <p className="w-1/2 bg-[#F9F5E8] text-[#082244] flex justify-center pt-14 my-6 px-6">
+      <div className="md:flex">
+        <img
+          src={pic}
+          alt="sbf logo"
+          className="w-1/2 bg-white h-[522px] md:block hidden"
+        />
+        <p className="md:w-1/2 bg-[#F9F5E8] text-[#082244] flex items-center flex-col pt-14 my-6 px-6 mb:pb-0 pb-6">
           The principle doctrine behind the Street bridge Foundation is social
           impact. To empower the marginalized 'Street Kids' who have been for
           long been flagged as the scape goats of every wrong doing on the
@@ -21,8 +25,8 @@ const Hear: React.FC<HearProps> = () => {
           it is, but we are curious of what the future holds for us. Just like
           us, so long as fellow youth immerse themselves in similar initiatives
           to impact the lives of those who deserve it most, so long Africa will
-          never stop moving forward. Viva Africa! <br /> By Herman Niyorurema, MD and
-          Founder
+          never stop moving forward. Viva Africa!
+          <p className="mt-3">By Herman Niyorurema, MD and Founder</p>
         </p>
       </div>
     </div>
--- src/components/about/Mission.tsx
+++ src/components/about/Mission.tsx
@@ -6,9 +6,9 @@ const Mission: React.FC<MissionProps> = () => {
   return (
     <div className="bg-[#082244] py-8 lg:px-24 px-6 pb-12">
       <div className="text-white mb-6">Our Mission</div>
-      <div className="flex">
-        <img src={logo} alt="sbf logo" className="w-1/2 bg-white h-[522px]"/>
-        <p className="w-1/2 bg-[#F9F5E8] text-[#082244] flex justify-center pt-14 my-6 px-6">
+      <div className="md:flex">
+        <img src={logo} alt="sbf logo" className="w-1/2 bg-white h-[522px] md:block hidden"/>
+        <p className="md:w-1/2 bg-[#F9F5E8] text-[#082244] flex justify-center pt-14 my-6 px-6 mb:pb-0 pb-6">
           Street Bridge Foundation mandates to impacting and empowering the
           lives of the marginalized kids who live on the street. In doing so, we
           take them back to school( If need be) or to career fields of interest.
